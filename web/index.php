ac543d4a014ebb18e29b88869a1068e6f0add1cd
Memo Cachú <gcachu.o@gmail.com>
2020-08-26 16:29:05 -0500

    test websockets

--- web/index.php
+++ web/index.php
@@ -1,1 +1,20 @@
 <?php
+?>
+<script>
+    const ws = new WebSocket('ws://127.0.0.1:8080');
+    ws.onopen = function(msg) {
+        // Logic for opened connection
+        console.log('Connection successfully opened');
+    };
+    ws.onmessage = function(msg) {
+        // Handle received data
+        console.log(msg);
+    };
+    ws.onclose = function(msg) {
+        // Logic for closed connection
+        console.log('Connection was closed.');
+    }
+    ws.error =function(err){
+        console.error(err); // Write errors to console
+    }
+</script>
